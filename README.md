6d12d695833bd0408021f7952eee115c3f1e961d
Gary Scriver <gary.scriver@savvas.com>
2021-04-08 14:19:36 -0600

    Updated to bananaj 0.7.0-SNAPSHOT

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ See [bananaj](https://github.com/banana-j/bananaj).
 
 [![GitHub license](https://img.shields.io/badge/license-MIT-lightgrey.svg)](https://github.com/gscriver/chimpexcel/raw/master/LICENSE)
 
+# Usage:
+java -jar chimpexcel.jar <_API_KEY_>
+
 # License
 MIT License
 
-Copyright (c) 2015 - 2019 Gary Scriver
+Copyright (c) 2015 - 2021 Gary Scriver
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- src/main/java/com/github/bananaj/chimpexcel/App.java
+++ src/main/java/com/github/bananaj/chimpexcel/App.java
@@ -5,10 +5,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
-import com.github.alexanderwe.bananaj.connection.MailChimpConnection;
-import com.github.alexanderwe.bananaj.model.list.MailChimpList;
-import com.github.alexanderwe.bananaj.model.list.member.Member;
-import com.github.alexanderwe.bananaj.utils.DateConverter;
+import com.github.bananaj.connection.MailChimpConnection;
+import com.github.bananaj.model.list.MailChimpList;
+import com.github.bananaj.model.list.member.Member;
+import com.github.bananaj.utils.DateConverter;
 
 import jxl.CellView;
 import jxl.Workbook;
@@ -46,87 +46,63 @@ public class App {
 	 */
 	public static void writeAllListToExcel(MailChimpConnection chimpCon, String filepath, boolean show_merge) throws Exception {
 		WritableWorkbook workbook = Workbook.createWorkbook(new File(filepath+".xls"));
-		WritableFont times16font = new WritableFont(WritableFont.TIMES, 16, WritableFont.BOLD, false);
-		WritableCellFormat times16format = new WritableCellFormat (times16font);
+		WritableFont times12font = new WritableFont(WritableFont.TIMES, 12, WritableFont.BOLD, false);
+		WritableCellFormat times12format = new WritableCellFormat (times12font);
 
 		List<MailChimpList> mailChimpLists = chimpCon.getLists(10,0);
 		int index  = 0;
 		for(MailChimpList mailChimpList : mailChimpLists){
 			WritableSheet sheet = workbook.createSheet(mailChimpList.getName(), index);
+			int hcolumn=0;
+			sheet.addCell(new Label(hcolumn++, 0, "MemberID", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "Email Address", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "Sign up", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "IP Sign up", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "Opt in", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "IP Opt in", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "Status", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "Avg. open rate", times12format));
+			sheet.addCell(new Label(hcolumn++, 0, "Avg. click rate", times12format));
 
-			Label memberIDLabel = new Label(0, 0, "MemberID",times16format);
-			Label email_addressLabel = new Label(1,0,"Email Address",times16format);
-			Label timestamp_sign_inLabel = new Label(2,0,"Sign up",times16format);
-			Label ip_signinLabel = new Label(3,0,"IP Sign up", times16format);
-			Label timestamp_opt_inLabel = new Label(4,0,"Opt in",times16format);
-			Label ip_optLabel = new Label(5,0,"IP Opt in", times16format);
-			Label statusLabel = new Label(6,0,"Status",times16format);
-			Label avg_open_rateLabel = new Label(7,0,"Avg. open rate",times16format);
-			Label avg_click_rateLabel = new Label(8,0,"Avg. click rate",times16format);
-
-
-			sheet.addCell(memberIDLabel);
-			sheet.addCell(email_addressLabel);
-			sheet.addCell(timestamp_sign_inLabel);
-			sheet.addCell(ip_signinLabel);
-			sheet.addCell(timestamp_opt_inLabel);
-			sheet.addCell(ip_optLabel);
-			sheet.addCell(statusLabel);
-			sheet.addCell(avg_open_rateLabel);
-			sheet.addCell(avg_click_rateLabel);
-
-			List<Member> members = mailChimpList.getMembers(0,0);
-			int merge_field_count = 0;
-
-			if (show_merge){
-				int last_column = 9;
-
-				Iterator<Entry<String, Object>> iter = members.get(0).getMergeFields().entrySet().iterator();
-				while (iter.hasNext()) {
-					Entry<String, Object> pair = iter.next();
-					sheet.addCell(new Label(last_column,0,pair.getKey(),times16format));
-					iter.remove(); // avoids a ConcurrentModificationException
-					last_column++;
-					merge_field_count++;
+			int row = 0;
+			for(Member member : mailChimpList.getMembers()) {
+				if (show_merge && row == 0) {
+					// add column headers
+					Iterator<Entry<String, Object>> iter = member.getMergeFields().entrySet().iterator();
+					while (iter.hasNext()) {
+						sheet.addCell(new Label(hcolumn++, 0, iter.next().getKey(), times12format));
+					}
 				}
-			}
+				
+				row++;
+				int rcolumn=0;
+				sheet.addCell(new Label(rcolumn++,row,member.getId()));
+				sheet.addCell(new Label(rcolumn++,row,member.getEmailAddress()));
+				sheet.addCell(new Label(rcolumn++,row,member.getTimestampSignup() != null ? DateConverter.toISO8601UTC(member.getTimestampSignup()) : ""));
+				sheet.addCell(new Label(rcolumn++,row,member.getIpSignup() != null ? member.getIpSignup() : ""));
+				sheet.addCell(new Label(rcolumn++,row,member.getTimestampOpt() != null ? DateConverter.toISO8601UTC(member.getTimestampOpt()) : ""));
+				sheet.addCell(new Label(rcolumn++,row,member.getIpOpt() != null ? member.getIpOpt() : ""));
+				sheet.addCell(new Label(rcolumn++,row,member.getStatus().toString()));
+				sheet.addCell(new Number(rcolumn++,row,member.getStats().getAvgOpenRate()));
+				sheet.addCell(new Number(rcolumn++,row,member.getStats().getAvgClickRate()));
 
-
-			for(int i = 0 ; i < members.size();i++)
-			{
-				Member member = members.get(i);
-				sheet.addCell(new Label(0,i+1,member.getId()));
-				sheet.addCell(new Label(1,i+1,member.getEmailAddress()));
-				sheet.addCell(new Label(2,i+1,member.getTimestampSignup() != null ? DateConverter.toISO8601UTC(member.getTimestampSignup()) : ""));
-				sheet.addCell(new Label(3,i+1,member.getIpSignup() != null ? member.getIpSignup() : ""));
-				sheet.addCell(new Label(4,i+1,member.getTimestampOpt() != null ? DateConverter.toISO8601UTC(member.getTimestampOpt()) : ""));
-				sheet.addCell(new Label(5,i+1,member.getIpOpt() != null ? member.getIpOpt() : ""));
-				sheet.addCell(new Label(6,i+1,member.getStatus().toString()));
-				sheet.addCell(new Number(7,i+1,member.getStats().getAvgOpenRate()));
-				sheet.addCell(new Number(8,i+1,member.getStats().getAvgClickRate()));
-
-				if (show_merge){
+				if (show_merge) {
 					//add merge fields values
-					int last_index = 9;
 					Iterator<Entry<String, Object>> iter_member = member.getMergeFields().entrySet().iterator();
 					while (iter_member.hasNext()) {
 						Entry<String, Object> pair = iter_member.next();
-						sheet.addCell(new Label(last_index,i+1,pair.getValue().toString()));
-						iter_member.remove(); // avoids a ConcurrentModificationException
-						last_index++;
-
+						sheet.addCell(new Label(rcolumn++,row,pair.getValue().toString()));
 					}
 				}
 			}
-
+			
 			CellView cell;
 
-			int column_count = 9 + merge_field_count;
-			for(int x=0;x<column_count;x++)
+			for(int i=0; i<hcolumn; i++)
 			{
-				cell=sheet.getColumnView(x);
+				cell=sheet.getColumnView(i);
 				cell.setAutosize(true);
-				sheet.setColumnView(x, cell);
+				sheet.setColumnView(i, cell);
 			}
 			index++;
 		}
